83c179647434b4cc33c5d798327906d4773bd7ef
Мазеин Николай Иванович <n.mazein@2gis.ru>
2023-03-01 01:48:03 +0700

    run in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/feynmaz/hex/internal/adapters/app/api"
 	"github.com/feynmaz/hex/internal/adapters/core/arithmetic"
+
+	"github.com/feynmaz/hex/internal/adapters/framework/left/grpc"
+	"github.com/feynmaz/hex/internal/adapters/framework/right/db"
 	"github.com/feynmaz/hex/internal/ports"
 )
 
 func main() {
-	// ports
-	var core ports.Arithmetic
+	var err error
 
-	core = arithmetic.NewAdapter()
-	res, err := core.Addition(1, 3)
+	// ports
+	var dbAdapter ports.DB
+	var core ports.Arithmetic
+	var appAdapter ports.API
+	var gRPCAdapter ports.GRPC
+
+	// right framework
+	dbDriver := os.Getenv("DB_DRIVER")
+	dsn := os.Getenv("DSN")
+	dbAdapter, err = db.NewAdapter(dbDriver, dsn)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to initiate db connection: %v", err)
 	}
-	println(res)
+	defer dbAdapter.CloseDBConnection()
+
+	// domain
+	core = arithmetic.NewAdapter()
+
+	// application
+	appAdapter = api.NewAdapter(dbAdapter, core)
+
+	// left framework
+	gRPCAdapter = grpc.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
